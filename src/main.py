6f18377e7290a39951cc82449bd92eedf2284423
CalilQ <calil_queiroz@hotmail.com>
2017-04-22 18:29:08 -0300

    Test sample streaming

--- src/main.py
+++ src/main.py
@@ -16,16 +16,18 @@ gain = 4
 scn = Scanner(sample_rate,gain)
 
 center_freq = 96.1e6
-samp_scale = 128
+smp_scale = 128
 
-scn.plot_psd(center_freq,samp_scale)
+scn.plot_psd(center_freq,smp_scale)
 
-f, pow_db = scn.calc_psd(center_freq,samp_scale)
+f, pow_db = scn.calc_psd(center_freq,smp_scale)
 
 print(np.max(pow_db))
 
-plt.plot(f,pow_db - np.min(pow_db))
+plt.plot(f,pow_db)
 plt.xlabel('frequency [Hz]')
 plt.ylabel('PSD [dB]')
 plt.grid(True)
 plt.show()
+
+scn.start_monitor(center_freq,smp_scale,50)
--- src/scanner.py
+++ src/scanner.py
@@ -11,6 +11,7 @@ from pylab import psd, xlabel, ylabel, show
 import scipy.signal as sig
 from rtlsdr import RtlSdr
 import numpy as np
+import asyncio as asy
 
 class Scanner(object):
     
@@ -44,4 +45,23 @@ class Scanner(object):
                               nperseg = 1024,return_onesided = False)
         
         return f,10*np.log10(pow_sd)
+    
+    
+    async def monitor_psd(self,fc,samp_scale,count_max):
+        self.sdr.center_freq = fc
+        count = 0
         
+        async for smpls in self.sdr.stream():
+            count = count + 1
+            f, pow_sd = sig.welch(smpls,fs=self.sdr.sample_rate,nfft=1024,\
+                              nperseg = 1024,return_onesided = False)
+            print(np.max(10*np.log10(pow_sd)))
+            
+            if count > count_max:
+                self.sdr.stop()
+                
+        self.sdr.close()
+        
+    def start_monitor(self,fc,samp_scale,count_max=10):
+        asy.get_event_loop().run_until_complete(self.monitor_psd(fc,\
+                          samp_scale,count_max))
